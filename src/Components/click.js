946a4c269d51b31c028b6557a09662d897d8a517
“Ashutosh <adityaashutosh06@gmail.com>
2023-07-29 15:17:15 +0530

    button fn

---
+++ src/Components/click.js
@@ -0,0 +1,12 @@
+
+import React,{Component} from "react";
+class ClassName extends Component{
+    render(){
+        return (
+            <div>
+                <button></button>
+            </div>
+        );
+    } 
+}
+export default ClassName;
